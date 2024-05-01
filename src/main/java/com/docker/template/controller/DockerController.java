b6de9d2fba06e0d75b26a1bd4048a515eb562264
Igor Nascimento <igor.nacimento51@gmail.com>
2024-05-01 16:42:08 -0300

    feat: add template

---
+++ src/main/java/com/docker/template/controller/DockerController.java
@@ -0,0 +1,16 @@
+package com.docker.template.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/docker")
+public class DockerController {
+
+    @GetMapping
+    public String helloToDocker() {
+        return "Hello Docker!";
+    }
+
+}
